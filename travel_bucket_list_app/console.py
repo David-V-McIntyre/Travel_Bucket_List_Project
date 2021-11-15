fabea22ddc4b9b67e41dae89b3f5d459528c2e45
David McIntyre <david.v.mcintyre@gmail.com>
2021-11-15 13:15:54 +0000

    repositories added

--- travel_bucket_list_app/console.py
+++ travel_bucket_list_app/console.py
@@ -1,1 +1,12 @@
 import pdb
+from models.city import City
+from models.country import Country
+
+import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
+
+city_repository.delete_all()
+country_repository.delete_all()
+
+country1 = Country("China")
+country_repository.save(country1)
--- travel_bucket_list_app/db/travel_bucketlist.sql
+++ travel_bucket_list_app/db/travel_bucketlist.sql
@@ -1,27 +1,15 @@
-DROP TABLE IF EXISTS countries;
 DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS countries;
 
 
 CREATE TABLE countries (
     id SERIAL PRIMARY KEY,
     country_name VARCHAR(255)
-    )
-
-
+);
 
 CREATE TABLE cities (
     id SERIAL PRIMARY KEY,
     city_name VARCHAR(255),
     country_id INT REFERENCES countries(id),
     visited BOOLEAN
-
-
-)
-    def __init__(self, name, country, visited = False, id= None):
-        self.name = name
-        self.country = country
-        self.visited = visited
-        self.id = id 
-
-    def mark_visited_city(self):
-        self.visited = True
+);
