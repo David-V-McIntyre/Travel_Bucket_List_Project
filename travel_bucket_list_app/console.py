5fb7cad5096a5a248a83f555862cc294bd838c1f
David McIntyre <david.v.mcintyre@gmail.com>
2021-11-15 13:57:09 +0000

    beggining cities_controller

--- travel_bucket_list_app/console.py
+++ travel_bucket_list_app/console.py
@@ -10,3 +10,10 @@ country_repository.delete_all()
 
 country1 = Country("China")
 country_repository.save(country1)
+country2 = Country("Canada")
+country_repository.save(country2)
+
+city1 = City("Beijing", country1)
+city_repository.save(city1)
+city2 = City("Toronto", country2)
+city_repository.save(city2)
--- travel_bucket_list_app/repositories/city_repository.py
+++ travel_bucket_list_app/repositories/city_repository.py
@@ -6,8 +6,8 @@ import repositories.country_repository as country_repository
 
 
 def save(city):
-    sql = "INSERT INTO cities (name, country_id) VALUES (%s, %s, %s) RETURNING *"
-    values = [city.name, city.country.id]
+    sql = "INSERT INTO cities (city_name, country_id, visited) VALUES (%s, %s, %s) RETURNING *"
+    values = [city.name, city.country.id, city.visited]
     results = run_sql(sql, values)
     id = results[0]['id']
     city.id = id
